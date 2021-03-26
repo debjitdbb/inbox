dc841da0db7870055e02b5149f2b55efc5e9ca5b
Debjit Datta <debjitdbb@gmail.com>
2021-03-26 12:09:23 +0530

    added latest version of solidity

--- deploy.js
+++ deploy.js
@@ -1,25 +1,45 @@
-const { interfaces } = require('mocha')
-const HDWalletProvider = require('truffle-hdwallet-provider')
+// const { interfaces } = require('mocha')
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const HDWalletProvider2 = require("truffle-hdwallet-provider");
+
 const Web3 = require('web3')
-const {interface,bytecode} = require('./compile')
+const compile = require('./compile');
+const interface = compile.contracts['inbox.sol'].Inbox.abi;
+const bytecode = compile.contracts['inbox.sol'].Inbox.evm.bytecode.object;
+const credentials = require('./config');
+const mnemonicPhrase = credentials.mnemonic;
+const endpoint = credentials.endpoint;
 
-const provider = new HDWalletProvider(
-    'blur blush post token toy dutch pipe wide frost knock oblige industry',
-    'https://rinkeby.infura.io/v3/d62ac338108c4854bfefca44ce317244'
+const provider = new HDWalletProvider({
+  mnemonic: {
+    phrase: mnemonicPhrase,
+  },
+  providerOrUrl:
+    endpoint,
+});
+
+
+const prov = new HDWalletProvider2(
+  mnemonicPhrase,
+  endpoint
 );
-
-const web3 = new Web3(provider);
+const web3 = new Web3(prov);
 
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
 
     console.log('Attempting to deploy from account',accounts[0]);
 
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({data: bytecode, arguments: ['Hi there!']})
-    .send({gas: '1000000',from: accounts[0]});
+    try{
+        const result = await new web3.eth.Contract(interface)
+          .deploy({ data: bytecode, arguments: ["Hi there!"] })
+          .send({from: accounts[0],gas:1000000});
+
+        console.log("Contract deployed to", result.options.address);
+    } catch(e){
+        console.log('error',e);
+    }
     
-    console.log('Contract deployed to',result.options.address);
 };
 
 deploy();
